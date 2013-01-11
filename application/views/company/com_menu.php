d3172e6bac3d7da8956aad8dda0eeb6cf71daf07
fara <farhatmihalko@gmail.com>
2013-01-11 19:02:23 +0600

    Profile changes

--- application/views/company/com_menu.php
+++ application/views/company/com_menu.php
@@ -1,54 +1,49 @@
-<div class="row  pad-tb">
-	<!--Information about company-->
-	<div class="twelve columns pad-bottom">
-		<?php
-			if(is_object($seller)){
-		?>
+<div class="page">
+	<div class="row  pad-tb">
+		<!--Information about company-->
+		<div class="twelve columns pad-bottom">
+			<?php
+				if(is_object($seller)){
+			?>
 
-			<a href="">
-				<?php
-					echo $seller->company_name;
-				?>
-			</a>
-			<a href="">
-				<?php
-					echo $seller->about;
-				?>
-			</a>
-			<a href="">
-				<?php
-					echo $seller->adress;
-				?>
-			</a>
-			<a href="">
-				<?php
-					echo $seller->phone1;
-					echo $seller->phone2;
-					echo $seller->phone3;
-				?>
-			</a>
-		<?php
-			}
-			else{
-		?>
+				<a href="">
+					<?php
+						echo $seller->company_name;
+					?>
+				</a>
+				<a href="">
+					<?php
+						echo $seller->about;
+					?>
+				</a>
+				<a href="">
+					<?php
+						echo $seller->adress;
+					?>
+				</a>
+				<a href="">
+					<?php
+						echo $seller->phone1;
+						echo $seller->phone2;
+						echo $seller->phone3;
+					?>
+				</a>
+			<?php
+				}
+				else{
+			?>
 
-			<a href="<?php echo base_url("index.php/main/company_info/".$id);?>">
-				Название компании :
-				<?php
-					echo $seller;
-				?>
-			</a>
+				<a href="<?php echo base_url("index.php/main/company_info/".$id);?>">
+					Название компании :
+					<?php
+						echo $seller;
+					?>
+				</a>
 
-		<?php
-			}
-		?>
-	</div>	
-	<!--end of information about company-->
-	<hr class="hr-dashed">
-	<!--Information about parts-->
-	<div class="twelve columns">
-		
+			<?php
+				}
+			?>
+		</div>	
+		<!--end of information about company-->
 	</div>
-	<!--end of parts information-->
-</div>
 
--- application/views/company/com_parts.php
+++ application/views/company/com_parts.php
@@ -1,71 +1,43 @@
-<div class="row">
-	<div class="twelve columns">
+<div class="row pad-bottom">
 
-		<div class="four columns">
-			
+	<!--header of products-->
+	<div class="twelve columns adv-line font-gray-bold  align-center adv-list-bordered">
+		<div class="five columns">
+			Название запчасти
 		</div>
-	
+		<div class="two columns">
+			Цена запчасти
+		</div>
+		<div class="five columns">
+			Комментарий компании
+		</div>
 	</div>
+	<!--end of header-->
+
+	<!--parts-->
+	<?php
+		foreach ($answers as $row){
+	?>
+	<div class="twelve columns adv-line hr-dashed">
+		<div class="five columns align-center">
+			<?php echo $queries[$row->query_id]->part; ?>
+			<span class="breadcum">
+				<?php echo $row->answer_date; ?>
+			</span>
+		</div>
+		<div class="two columns align-center">
+			<?php echo $row->price;?> тенге
+		</div>
+		<div class="five columns align-center">
+			<?php echo $row->comment; ?>
+		</div>
+	</div>
+	<?php
+		}
+	?>
+	<!--end of parts-->
+
 </div>
 
 
-
-<div class='row'>
-	<hr>
-			<table style='width : 100%;'>
-				<tbody style='width : 90%;  text-align : center;margin:0 auto; font-size : 13px;'>
-					<tr class='t_query-main'>
-									<td colspan='7' style='background:white;'>
-										<div class='t_query-content'>
-											<div class='t_q-c-header'>
-												<div class='name'>
-													Название запчасти
-												</div>
-												<div class='salary'>
-													Цена запчасти
-												</div>
-												<div class='comment'>
-													Комментарий к товару
-												</div>
-												<div class='action'>
-													Дата
-												</div>
-
-											</div>
-											<div class='clearfix'></div>
-											<?php 
-											$counter=1;
-											foreach ($answers as $row) { ?>
-
-
-											<div class='t_q-c-line <?php if ($counter++==1) echo 't_q-c-l-first';?>'>
-												<div class='t_q-c-l-i'>
-													<div class='marginTop20'>
-														<a href='#' class='btn'><i class='icon-bookmark-empty'></i> <?php echo $queries[$row->query_id]->part; ?></a>
-													</div>
-												</div>
-												<div class='t_q-c-l-s'>
-													<p class='t_q-c-s-tenge'><?php echo $row->price;?><span>тенге</span></p>
-												</div>
-												<div class='t_q-c-l-c'>
-													<p>
-													<?php echo $row->comment; ?>
-													</p>
-												</div>
-												<div class='t_q-c-l-a'>
-													<p>
-													<?php echo $row->answer_date; ?>
-													</p>
-												</div>
-												
-											</div>
-											<?php } ?>
-										
-											
-										</div>
-									</td>
-								</tr>
-				</tbody>
-			</table>
-	</div>
-	
+</div>
