9f180894aeae9d103627a600454942cdaf7d551c
fara <farhatmihalko@gmail.com>
2013-01-11 23:50:33 +0600

    Radio buttonts add

--- application/views/main-login.php
+++ application/views/main-login.php
@@ -10,16 +10,29 @@
 				?>
 			</h5>
 			<form action="" method="POST">
-				<input type="text" placeholder="Введите ваше имя сюда" class="line-after-big line-before-big" name="name">
-				<input type="password" placeholder="Тут надо написать пароль" class="line-after-big" name="password">
-				<input type="radio" id="label_remember" name="remember"><label for="label_remember">&nbsp;Я продавец</label>
-				<a href="<?php echo base_url();?>index.php/main/restore_password" class="right font-gray-bold"><i class="icon-refresh pad-right"></i>Забыл пароль</a>
-				<div class="line-before pad"></div>
+				<input type="text" placeholder="Введите ваше имя сюда" 
+				class="line-after-big line-before-big" name="name">
+				<input type="password" placeholder="Тут надо написать пароль" 
+				class="line-after-big" name="password">
+				<input type="checkbox" id="checkbox-1-1" class="regular-checkbox" 
+				name="remember">
+				<label for="checkbox-1-1" style="top:4px;"></label>
+				<label for="checkbox-1-1">
+					&nbsp;Я продавец
+				</label>
+				<a href="<?php echo base_url();?>index.php/main/restore_password" 
+					class="right font-gray-bold">
+					<i class="icon-refresh pad-right"></i>Забыл пароль
+				</a>
+				<div class="line-before pad">
+					
+				</div>
 				<div class="center pad">
 					<button type="submit" class="btn btn-green"><i class="icon-key pad-right"></i>Войти в кабинет</button>	
 					<a href="<?php echo base_url();?>index.php/main/reg_user" class="btn inline btn-green "><i class="icon-user pad-right"></i>Создать новый аккаунт</a>	
 				</div>
 			</form>
+
 		</div>
 	<!--end of page content-->
 	</div>
